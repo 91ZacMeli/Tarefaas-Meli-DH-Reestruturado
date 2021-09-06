4db4b5187b734e2124350b017419cf3fce4450d3
MyMeliMail <isac.cordeiro@mercadolivre.com>
2021-09-06 19:25:54 -0300

    exempolos exccoes vistos em aula

---
+++ revisaoPOO/src/exemplo3/DoblrFormatado.java
@@ -0,0 +1,9 @@
+package exemplo3;
+
+import java.text.DecimalFormat;
+
+public class DoblrFormatado {
+    public void imprime(double valor){
+        DecimalFormat df = new DecimalFormat("#.##");
+    }
+}
